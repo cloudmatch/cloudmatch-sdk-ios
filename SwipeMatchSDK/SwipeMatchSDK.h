09a39cd353b29753b8e6df9561a6cd69af4fa39c
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-01-16 14:07:49 +0100

    Initial Commit

---
+++ SwipeMatchSDK/SwipeMatchSDK.h
@@ -0,0 +1,13 @@
+//
+//  SwipeMatchSDK.h
+//  SwipeMatchSDK
+//
+//  Created by Giovanni on 1/16/14.
+//  Copyright (c) 2014 LimeBamboo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SwipeMatchSDK : NSObject
+
+@end
---
+++ SwipeMatchSDK/SwipeMatchSDK.m
@@ -0,0 +1,13 @@
+//
+//  SwipeMatchSDK.m
+//  SwipeMatchSDK
+//
+//  Created by Giovanni on 1/16/14.
+//  Copyright (c) 2014 LimeBamboo. All rights reserved.
+//
+
+#import "SwipeMatchSDK.h"
+
+@implementation SwipeMatchSDK
+
+@end
