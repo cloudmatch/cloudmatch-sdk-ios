fd30a26c02d28883f2b7fca07f87cd5e9ccca660
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-01-22 11:47:07 +0100

    moved Apple Frameworks out of our SDK. Added pragma message in case CFNetwork linking not detected

--- SwipeMatchSDK/SwipeMatchSDK.h
+++ SwipeMatchSDK/SwipeMatchSDK.h
@@ -6,9 +6,15 @@
 //  Copyright (c) 2014 LimeBamboo. All rights reserved.
 //
 
+#ifndef __CFNETWORK__
+#pragma message("CFNetwork framework not found in project, or not included in precompiled header. Be sure to include all required frameworks for SwipeMatchSDK.")
+#endif
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import "SMSwipeMatchClient.h"
+
+
 
 @interface SwipeMatchSDK : NSObject
 
