788d4561fc77dae554eddaf4f6573deafc42ec97
Fabio Tiriticco <fabio.europe@gmail.com>
2014-04-11 17:43:54 +0200

    restored endpoint value

--- GestureMatchSDK/GestureMatch/Handlers/GMServerMessagesHandler.m
+++ GestureMatchSDK/GestureMatch/Handlers/GMServerMessagesHandler.m
@@ -48,6 +48,11 @@
 - (void)onServerMessage:(NSString *)message
 {
     @try {
+        if ([message isEqualToString:@"k"]) {
+            // keepalive message
+            return;
+        }
+        
         NSError *jsonError = nil;
         NSDictionary* msgJson = [NSJSONSerialization JSONObjectWithData:[message dataUsingEncoding:NSUTF8StringEncoding] options:0 error:&jsonError];
         if ([msgJson objectForKey:KIND] == nil) {
